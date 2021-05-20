496ceda95ff035fd47e728d3078341e5ce9c37fb
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-05-20 15:12:43 +0200

    Initial app version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
+
+import marked from "marked";
+import ReactHtmlParser from "html-react-parser";
 
 function App() {
+  const [text, setText] = useState(defaultText);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  const marked = require("marked");
+  marked.setOptions({ breaks: true });
+
+  let test = marked(text);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="edit">
+        <div className="topbar">AYY</div>
+        <textarea onChange={handleChange} value={text} id="editor" />
+      </div>
+      <div id="preview">
+        <h3>Preview</h3>
+        <div>{ReactHtmlParser(test)}</div>
+      </div>
     </div>
   );
 }
+
+const defaultText = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
 
 export default App;
