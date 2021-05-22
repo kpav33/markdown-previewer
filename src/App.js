2dd8baa4f804cbf9c5d69682177262950f778eb2
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-05-22 13:36:14 +0200

    Some style changes, begin implementing fullscreen option

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import ReactHtmlParser from "html-react-parser";
 function App() {
   const [text, setText] = useState(defaultText);
 
+  const fullScreenStyle = {
+    color: "blue",
+    gridTemplate: "1fr 1fr / auto",
+  };
+
   function handleChange(e) {
     const { value } = e.target;
     setText(value);
@@ -20,7 +25,13 @@ function App() {
     <div className="wrapper">
       <div id="edit" className="panel">
         <div className="topbar">
-          <i class="ri-pencil-line"></i>Editor
+          <div className="title">
+            <i class="ri-pencil-line"></i>
+            Editor
+          </div>
+          <button onClick={() => console.log("OK")}>
+            <i class="ri-fullscreen-line"></i>
+          </button>
         </div>
         <textarea
           onChange={handleChange}
@@ -31,7 +42,12 @@ function App() {
       </div>
       <div id="preview-panel" className="panel">
         <div className="topbar">
-          <i class="ri-file-search-line"></i>Previewer
+          <div className="title">
+            <i class="ri-file-search-line"></i>Previewer
+          </div>
+          <button onClick={() => console.log("OK")}>
+            <i class="ri-fullscreen-exit-line"></i>
+          </button>
         </div>
         <div id="preview" className="preview">
           {ReactHtmlParser(test)}
@@ -41,7 +57,14 @@ function App() {
   );
 }
 
-const defaultText = `
+const defaultText = `# Markdown Previewer
+
+This project was built as part of **freeCodeCamp Front End Libraries Projects**.
+
+You can see the source code for this project in the [project's GitHub repository](https://github.com/kpav33/markdown-previewer) and you can read the instructions for the project on the [freeCodeCamp project page](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer).
+
+I've used [marked](https://github.com/markedjs/marked) together with [html-react-parser](https://github.com/remarkablemark/html-react-parser) to complete this project.
+
 # Welcome to my React Markdown Previewer!
 
 ## This is a sub-heading...
@@ -65,7 +88,9 @@ Or... wait for it... **_both!_**
 And feel free to go crazy ~~crossing stuff out~~.
 
 There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
+> Block Quotes! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus nisl leo, eget porttitor eros fermentum vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+>
+>> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus nisl leo, eget porttitor eros fermentum vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
 
 And if you want to get really crazy, even tables:
 
@@ -73,6 +98,7 @@ Wild Header | Crazy Header | Another Header?
 ------------ | ------------- | -------------
 Your content can | be here, and it | can be here....
 And here. | Okay. | I think we get it.
+Another one. | Getting a bit long. | We should probably stop.
 
 - And of course there are lists.
   - Some are bulleted.
@@ -80,11 +106,10 @@ And here. | Okay. | I think we get it.
         - That look like this.
 
 
-1. And there are numbererd lists too.
+1. And there are numbered lists too.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![React Logo w/ Text](https://goo.gl/Umyytc)
-`;
+![freeCodeCamp Logo](https://upload.wikimedia.org/wikipedia/commons/3/39/FreeCodeCamp_logo.png)`;
 
 export default App;
